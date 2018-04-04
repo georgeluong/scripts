e0132100ad15d6656cd5d5c8cd94714409470ecc
George Luong <gluong@slack-corp.com>
2018-04-04 09:38:57 -0700

    -h help menu and rate limits

--- slack-delete-files-by-channel.py
+++ slack-delete-files-by-channel.py
@@ -3,6 +3,7 @@
 import click
 import json
 import requests
+import time
 
 
 def list_file_ids(channel, token, count, print_len=False):
@@ -24,9 +25,11 @@ def list_file_ids(channel, token, count, print_len=False):
     return file_ids
 
 
-def delete_files(token, file_ids):
+def delete_files(token, file_ids, rate_limit):
     """
     Pass in file IDs and use the files.delete API method to delete a file.
+    
+    Uses rate_limit to pause between each API call.
     """
     uri = 'https://slack.com/api/files.delete'
     for file_id in file_ids:
@@ -34,9 +37,15 @@ def delete_files(token, file_ids):
         print "Deleting file {}".format(file_id)
         response = requests.post(uri, params=params)
         print "Status code: {}".format(response.status_code)
+        if rate_limit:
+            time.sleep(rate_limit)
 
 
-@click.command()
+# By default '-h' doesn't give print help.
+# http://click.pocoo.org/5/documentation/#help-parameter-customization
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS)
+
 @click.option('--token', '-t', required=True, help=("A user token, which you can get"
               "from https://api.slack.com/custom-integrations/legacy-tokens."
               "Be absolutely certain to not share this token with anyone.")
@@ -45,10 +54,11 @@ def delete_files(token, file_ids):
 @click.option('--count', default=1000, help='Number of files to delete at once.')
 @click.option('--delete', default=False, is_flag=True, help='Set --delete if you want to actually delete files.')
 @click.option('--print', 'print_len', is_flag=True, flag_value=True, default=True, help='Set --print to see number of files.')
-def main(token, channel, count, print_len, delete):
+@click.option('--rate-limit', '-r', default=1, help='Time to wait between each API call. Reference the rate limits here: https://api.slack.com/docs/rate-limits#tiers.')
+def main(token, channel, count, print_len, delete, rate_limit):
     file_ids = list_file_ids(channel, token, count, print_len)
     if delete:
-        delete_files(token, file_ids)
+        delete_files(token, file_ids, rate_limit)
 
 
 if __name__ == "__main__":
